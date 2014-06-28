d83a5724123870276083a3fd749ff2a05e5341ca
Jesus Vieira <jesusvieiradelima@gmail.com>
2014-06-28 15:19:15 -0400

    configurando abstract_factories

--- config/module.config.php
+++ config/module.config.php
@@ -9,6 +9,10 @@ return array(
         )
     ),
     'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
         'factories' => array(
             'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
         ),
